501e7008dea68f1167be409027f9da9eefc4016f
Bruno de Pádua Allegretti Sousa <brunoifspcar@gmail.com>
2025-11-25 23:29:58 -0300

    Mudanças no backend pat3

--- routes/auth.js
+++ routes/auth.js
@@ -1,50 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const User = require('../models/User');
+const authController = require('../controllers/authController');
 
 // Config do multer para foto de perfil
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.post('/register', upload.single('profilePicture'), async (req, res) => {
-  try {
-    const {
-      name,
-      email,
-      password,
-      cep,
-      street,
-      number,
-      neighborhood,
-      city,
-      state
-    } = req.body;
+router.post('/register', upload.single('profilePicture'), authController.register);
 
-    const profilePicture = req.file ? req.file.buffer : null;
-
-    const newUser = new User({
-      name,
-      email,
-      password,
-      address: {
-        cep,
-        street,
-        number,
-        neighborhood,
-        city,
-        state
-      },
-      profilePicture
-    });
-
-    await newUser.save();
-
-    res.json({ msg: 'Usuário criado com sucesso!' });
-  } catch (error) {
-    console.error("Erro no register:", error);
-    res.status(500).json({ msg: 'Erro ao registrar.' });
-  }
-});
+router.post('/login', authController.login);
 
 module.exports = router;
